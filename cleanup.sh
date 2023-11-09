494651c25f9e87b0d7cc6646f3fb1d424717d3b5
Ubuntu <ubuntu@ip-172-31-47-147.eu-west-2.compute.internal>
2023-11-09 15:58:00 +0000

    created jenkinsfile

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,8 @@ docker rmi -f $(docker images -a -q)
 docker volume rm -f $(docker volume ls -q)
 docker network rm -f $(docker network ls -q)
 
+docker builder prune --all --force
+
 docker images -a
 docker network ls
 docker ps -a
